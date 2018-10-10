2669e46b97520e4e3d71747669063af23926b804
sergey.topolov <sergey.topolov@megafon.ru>
2018-10-10 17:58:49 +0300

    07

--- src/workbook.js
+++ src/workbook.js
@@ -5,42 +5,118 @@ const workbook = new Excel.Workbook();
 const date = new Date().toLocaleString().slice(0, 10);
 const title = `Отчет по кэшбэкам (${date})`;
 
-function addHeader(sheet, title) {
-    _.each([1, 2, 3, 4, 5, 6], index => {
-        sheet.getColumn(index).width = 18;
+const range = r => _.range(1, r, 1);
+
+function addHeader(sheet, title, colsCount) {
+    _.each(range(colsCount + 2), index => {
+        if(index === 1) sheet.getColumn(index).width = 30;
+        else sheet.getColumn(index).width = 18;
     });
     mergeCells(sheet, [
-        ['A1', 'E1'],
+        ['A1', 'F1'],
     ]);
     const A1 = sheet.getCell('A1');
     A1.value = title;
-    A1.alignment = {horizontal: 'center', vertical: 'center'};
-    sheet.getRow(A1._row._number).height = 30;
+    A1.alignment = {horizontal: 'center', vertical: 'middle'};
+    sheet.getRow(1).height = 30;
+    sheet.getRow(1).eachCell(cell => {
+        cell.font = {
+            ...cell.font,
+            bold: true
+        };
+    });
+    sheet.addRow(['']);
 }
 
 function addTableHeader(sheet, lists) {
     const sites = _.map(lists, 'fileName');
-    sheet.addRow(['XXX', 'megafon', ...sites]);
+    sheet.addRow(['', ...sites]);
+    const currentRow = sheet.getRow(sheet.rowCount);
+
+    currentRow.alignment = {horizontal: 'center', vertical: 'middle'};
+    currentRow.height = 30;
+    currentRow.eachCell(cell => {
+        cell.font = {
+            ...cell.font,
+            bold: true
+        };
+    });
 }
 
 function addTable(sheet, data) {
-    _.each(data, (shop, shopIndex) => {
-        sheet.addRow([shopIndex, 'МЕГАФОН', ...shop]);
-    })
+    const firstRow = sheet.lastRow + 1;
+    firstRow.height = 50;
+    _.each(data, shop => {
+        const {title, value} = shop;
+        sheet.addRow([title, ...value]);
+    });
+    // sheet.eachRow({includeEmpty: true}, (row, rowNumber) => {
+    //     // if(rowNumber >= firstRow) {
+    //         row.eachCell((cell, colIndex) => {
+    //             // if(colIndex > 1) {
+    //                 // currentRow.alignment = {horizontal: 'center'};
+    //             // }
+    //         });
+    //     // }
+    // });
 }
 
 const buildReport = (data, lists) => {
     const sheet = workbook.addWorksheet('Отчет');
-    addHeader(sheet, title);
+    const colsCount = lists.length;
+    addHeader(sheet, title, colsCount);
     addTableHeader(sheet, lists);
     addTable(sheet, data);
     workbook.xlsx.writeFile(`${title}.xlsx`);
 }
-
+//================== heplers =================
 function mergeCells(sheet, cellsRangeMap) {
     _.each(cellsRangeMap, range => {
         sheet.mergeCells(range[0], range[1]);
     });
+}
+
+function fillCelsBG(sheet, cells, colors) {
+    const baseFillOptions = {
+        type: 'pattern',
+        pattern: 'solid'
+    };
+    eachCell(sheet, cells, (cell, cellIndex) => {
+        const color = colors[cellIndex];
+        cell.fill = {
+            ...baseFillOptions,
+            fgColor: {argb: color}
+        };
+    });
+}
+
+function setCellBorder(sheet, cells, style, sides) {
+    const obj = {};
+    _.each(sides, side => {
+        obj[side] = {style};
+    });
+    eachCell(sheet, cells, cell => {
+        cell.border = obj;
+    });
+}
+
+function eachCell(sheet, cells, impl) {
+    if(_.isObject(cells)) {
+        impl(cells);
+    }
+    if(_.isString(cells)) {
+        impl(sheet.getCell(cells));
+    }
+    if(_.isArray(cells)) {
+        _.each(cells, cell => {
+            if(_.isObject(cell)) {
+                impl(cell);
+            }
+            if(_.isString(cell)) {
+                impl(sheet.getCell(cell));
+            }
+        });
+    }
 }
 
 module.exports = buildReport;
