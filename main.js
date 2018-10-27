c5287538c6156e30fa34b72a8cd7ddef4442a31c
topus009 <sj_89@mail.ru>
2018-10-27 14:26:02 +0300

    18

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron');
+const parser = require('./src/index.js');
+
+let win;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  let contents = win.webContents;
+  contents.openDevTools();
+  win.on('closed', function() {
+    win = null;
+  });
+  contents.executeJavaScript(parser).then(res => {
+    console.log({res});
+  });
+});
